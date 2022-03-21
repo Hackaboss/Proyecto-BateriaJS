fea2064d12f282a83763a5448245ad087430c8ba
Tu Nombre <you@example.com>
2022-03-21 17:35:03 +0100

    js

--- script.js
+++ script.js
@@ -2,78 +2,78 @@
 
 // Crash
 // Creamos el nuevo HTMLAudioElement
-var audioCrash = new Audio("./sounds/crash.wav");
+const audioCrash = new Audio("./sounds/crash.wav");
 
 // Creamos la constante crash y seleccionamos en el DOM el elemento correspondiente a crash
 const crash = document.querySelector("div:first-of-type");
 
 // Creamos un evento para que el sonido crash se ejecute cuando clicamos sobre él
 crash.addEventListener("click", (event) => {
-  audioCrash.play();
+  play(audioCrash);
 });
 
 // HiHat Close
-var audioHiHatClose = new Audio("./sounds/hihat-close.wav");
+const audioHiHatClose = new Audio("./sounds/hihat-close.wav");
 const hhClose = document.querySelector("div:nth-of-type(2)");
 
 hhClose.addEventListener("click", (event) => {
-  audioHiHatClose.play();
+  play(audioHiHatClose);
 });
 
 // HiHat Open
-var audioHiHatOpen = new Audio("./sounds/hihat-open.wav");
+const audioHiHatOpen = new Audio("./sounds/hihat-open.wav");
 const hhOpen = document.querySelector("div:nth-of-type(3)");
 
 hhOpen.addEventListener("click", (event) => {
-  audioHiHatOpen.play();
+  play(audioHiHatOpen);
 });
 
 // Kick
-var audioKick = new Audio("./sounds/kick.wav");
+const audioKick = new Audio("./sounds/kick.wav");
 const kick = document.querySelector("div:nth-of-type(4)");
 
 kick.addEventListener("click", (event) => {
-  audioKick.play();
+  play(audioKick);
 });
 
 // Ride
-var audioRide = new Audio("./sounds/ride.wav");
+const audioRide = new Audio("./sounds/ride.wav");
 const ride = document.querySelector("div:nth-of-type(5)");
 
 ride.addEventListener("click", (event) => {
-  audioRide.play();
+  play(audioRide);
 });
 
 // Snare
-var audioSnare = new Audio("./sounds/snare.wav");
+const audioSnare = new Audio("./sounds/snare.wav");
 const snare = document.querySelector("div:nth-of-type(6)");
 
 snare.addEventListener("click", (event) => {
-  audioSnare.play();
+  play(audioSnare);
 });
 
 // Tom High
-var audioTomHigh = new Audio("./sounds/tom-high.wav");
+const audioTomHigh = new Audio("./sounds/tom-high.wav");
 const tomHigh = document.querySelector("div:nth-of-type(7)");
 
 tomHigh.addEventListener("click", (event) => {
-  audioTomHigh.play();
+  play(audioTomHigh);
 });
 
 // Tom Mid
-var audioTomMid = new Audio("./sounds/tom-mid.wav");
+const audioTomMid = new Audio("./sounds/tom-mid.wav");
 const tomMid = document.querySelector("div:nth-of-type(8)");
 
 tomMid.addEventListener("click", (event) => {
-  audioTomMid.play();
+  play(audioTomMid);
 });
 
 // Tom Low
-var audioTomLow = new Audio("./sounds/tom-low.wav");
+const audioTomLow = new Audio("./sounds/tom-low.wav");
 const tomLow = document.querySelector("div:last-of-type");
 
 tomLow.addEventListener("click", (event) => {
-  audioTomLow.play();
+  play(audioTomLow);
 });
 
 // Eventos de teclado
@@ -81,30 +81,36 @@ tomLow.addEventListener("click", (event) => {
 document.addEventListener("keydown", (event) => {
   // audioCrash.play(event);
   if (event.code === "KeyC") {
-    audioCrash.play();
+    play(audioCrash);
   }
   if (event.code === "KeyG") {
-    audioHiHatClose.play();
+    play(audioHiHatClose);
   }
   if (event.code === "KeyO") {
-    audioHiHatOpen.play();
+    play(audioHiHatOpen);
   }
   if (event.code === "KeyK") {
-    audioKick.play();
+    play(audioKick);
   }
   if (event.code === "KeyR") {
-    audioRide.play();
+    play(audioRide);
   }
   if (event.code === "KeyS") {
-    audioSnare.play();
+    play(audioSnare);
   }
   if (event.code === "KeyH") {
-    audioTomHigh.play();
+    play(audioTomHigh);
   }
   if (event.code === "KeyM") {
-    audioTomMid.play();
+    play(audioTomMid);
   }
   if (event.code === "KeyL") {
-    audioTomLow.play();
+    play(audioTomLow);
   }
 });
+
+function play(sound) {
+  setTimeout(() => {
+    sound.play();
+  }, 150);
+}
